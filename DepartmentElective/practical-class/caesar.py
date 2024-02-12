c7098b0282a0943b7d40459efdc6e6e8cba8f704
Hail0Hydra <thehokage0hiddenleaf@gmail.com>
2024-02-12 23:39:26 +0530

    done:  ROT cipher programs

--- DepartmentElective/practical-class/caesar.py
+++ DepartmentElective/practical-class/caesar.py
@@ -1,21 +1,58 @@
-import sys
+'''
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+AUTHOR:            hail0hydra
+DESCRIPTION:       simple rot cypher or modern caesar cipher in python
+DATE:              02 Feb 2024
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''
 
-pt = sys.argv[1].lower()
-shift = int(sys.argv[2])
+
+import argparse
+
+
+
+print("\n\n\t\033[0;34m --==[[ MODERN CAESAR CYPHER || ROT (n) CYPHER ]]==--\n\n\033[0m")
+
+parser = argparse.ArgumentParser(description="caesar cypher")
+
+parser.add_argument('mode', type=str, help="e - encrypt, d - decrypt")
+
+args = parser.parse_args()
+
 
 def caesar(pt, s):
 
     ct = str()
     for i in pt:
-        # if ord(i) == ord('z') or ord(i) == ord('Z'):
-        #     ct+= ord('a') + s
-        # else:
-        ct += chr((ord(i) - ord('a')) + s)
-
+        if i == " ":
+            ct += " "
+        elif i.islower():
+            ct += chr((((ord(i) - ord('a')) + s ) % 26 ) + ord('a'))
+        elif i.isupper():
+            ct += chr((((ord(i) - ord('A')) + s ) % 26 ) + ord('A'))
     return ct
 
 
-print(caesar(pt,shift))
+def decaesar(ct, s):
+
+    pt = str()
+    for i in ct:
+        if i == " ":
+            pt += " "
+        elif i.islower():
+            pt += chr((((ord(i) - ord('a')) - s ) % 26 ) + ord('a'))
+        elif i.isupper():
+            pt += chr((((ord(i) - ord('A')) - s ) % 26 ) + ord('A'))
+    return pt
 
 
-    
+if __name__ == "__main__":
+        if args.mode == 'e':
+            pt = input("Enter plain text: ")
+            shift = int(input("Enter shift value(int): "))
+            print(f"\nCypher text is: {caesar(pt,shift)}\n")
+        elif args.mode == 'd':
+            ct = input("Enter cypher text: ")
+            shift = int(input("Enter shift value(int): "))
+            print(f"\nPlain text is: {decaesar(ct,shift)}\n")
+
--- DepartmentElective/practical-class/ceasar.c
+++ DepartmentElective/practical-class/ceasar.c
@@ -1,31 +1,108 @@
-#include <stdio.h>
-# include <string.h>
 
+/*
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+AUTHOR:            hail0hydra
+DESCRIPTION:       simple rot cypher or modern caesar cipher in python
+DATE:              02 Feb 2024
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+char* caesar(char* pt, int s);
+char* decaesar(char* pt, int s);
 
 int main(int argc, char *argv[])
 {
 
+
+    printf("\033[0;34m");
+    printf("\n\n\t--==[[ MODERN CAESAR CIPHER || ROT (n) CIPHER ]]==--\n\n");
+    printf("\033[0m");
+
     if (argc < 2){
-        printf("\n\nUsage ./a.exe {number of shifts}\n\n");
+        printf("\n\nUsage ./%s {mode} [e - encrypt, d - decrypt]\n\n",argv[0]);
+        return 1;
     };
 
-    int shift = atoi(argv[1]);
-    char s[100];
-    printf("Enter a string: ");
-    scanf("%s",&s);
 
-    printf("\n\nthe string user entred is : %s\n", s);
+    if (strcmp(argv[1],"e") == 0){
 
-    char e[100];
+        char pt[100];
+        int s = 0;
+        printf("\nenter plaintext: ");
+        gets(pt);
+        printf("\nenter shift value: ");
+        scanf_s("%d",&s);
+        char* res = caesar(pt,s);
+        printf("\n\nCipher text is %s",res);
+        free(res);
+    }
+    else if (strcmp(argv[1], "d") == 0){
 
-    for (int i=0; i<strlen(s); i++){
-        e[i] = s[i] + shift;
+        char ct[100];
+        int s = 0;
+        printf("\nenter cipher-text: ");
+        gets(ct);
+        printf("\nenter shift value: ");
+        scanf_s("%d",&s);
+        char* res = decaesar(ct,s);
+        printf("\n\nCipher text is %s",res);
+        free(res);
     };
 
-    printf("The value of encrypted text is: %s", e);
 
     return 0;
 }
 
+char* caesar(char* pt, int s){
+
+    int l = strlen(pt);
+    char* e = (char*)malloc((l + 1) * sizeof(char));
 
 
+    for (int i=0; i<strlen(pt); i++){
+
+        
+        if (pt[i] == ' '){
+            e[i] = ' ';
+        }
+        if (islower(pt[i])){
+            e[i] = (((pt[i] - 'a') + s) % 26 ) + 'a';
+        }
+        else if (isupper(pt[i])){
+            e[i] = (((pt[i] - 'A') + s) % 26 ) + 'A';
+        }
+        
+    }
+
+    e[l] = '\0';
+    return e;
+}
+
+char* decaesar(char* pt, int s){
+
+    int l = strlen(pt);
+    char* e = (char*)malloc((l + 1) * sizeof(char));
+
+
+    for (int i=0; i<strlen(pt); i++){
+
+        if (pt[i] == ' '){
+            e[i] = ' ';
+        }
+        else if (islower(pt[i])){
+            e[i] = (((pt[i] - 'a') - s + 26) % 26 ) + 'a';
+        }
+        else if (isupper(pt[i])){
+            e[i] = (((pt[i] - 'A') - s + 26) % 26 ) + 'A';
+        };
+    };
+
+    e[l] = '\0';
+    return e;
+}
